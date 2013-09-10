ace88695564323fd9a2cabb37453a83c67d9d26f
Steve B. <steve_beauge@msn.com>
2013-09-10 10:07:56 +0200

    0.1
    First working version, with hard-coded paths

---
+++ hypervbackup.ps1
@@ -0,0 +1,18 @@
+﻿$date = Get-Date -Format "yyyyMMdd"
+
+$s1 = (gwmi -List Win32_ShadowCopy).Create("d:\", "ClientAccessible")
+$s2 = gwmi Win32_ShadowCopy | ? { $_.ID -eq $s1.ShadowID }
+$d  = $s2.DeviceObject + "\"
+cmd /c mklink /d d:\shadowcopy "$d"
+
+$targetDir = "N:\vmbackup\$date"
+
+if(-not (Test-Path $targetDir)) { 
+    New-Item -ItemType Folder -Path $targetDir
+}
+
+robocopy D:\shadowcopy\Hyper-V "$targetDir" /e /np
+
+"vssadmin delete shadows /Shadow=""$($s2.ID.ToLower())"" /Quiet" | iex
+
+Remove-Item d:\shadowcopy -Confirm:$false -Force
